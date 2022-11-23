ad9952469c5921af5bf28bb85165d202db086c7b
serpentaire <glemoine@hotmail.fr>
2022-11-23 17:16:00 +0100

    ajout dependance

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import Home from "@pages/Home";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
-    </div>
+    <Router>
+      <div className="App">
+        <Home />
+        <p>coucou</p>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
