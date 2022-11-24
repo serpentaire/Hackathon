93e3559472fbf722f7ffd137db4fa6b645304bff
arouxel17 <alexandrerouxel17@gmail.com>
2022-11-24 15:25:40 +0100

    add de la page css de confirmpage

--- frontend/src/pages/ConfirmPage/ConfirmPage.jsx
+++ frontend/src/pages/ConfirmPage/ConfirmPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import "../ConfirmPage/ConfirmPage.css";
 
 function ConfirmPage () {
 
    return(
-    <div className="container">
-        <div className="confirmation-message text-center">
-            <h1>Nous avons bien reçu votre paiement, votre séjour va être préparé par nos soins ! 🌞</h1>
+    <div className="container-confirm">
+        <div className="confirmation-message text-center col-12">
+            <h1>Nous avons bien reçu votre paiement, 
+                <br /> votre séjour va être préparé par nos soins ! 🌞</h1>
         </div>
     </div>
    ) 
