c82855de387b00644f2653d06a925e2395fc007e
MUSTAPHA SAID <mustaphasaid.pr@gmail.com>
2022-11-24 12:25:32 +0100

    data done

---
+++ frontend/src/services/Data.js
@@ -0,0 +1,73 @@
+const beers = [
+  {
+    id: 1,
+    name: "DoubleTree by Hilton Doha - Al Sadd",
+    adress: "65 Mohamed Bin Al Qassim St, Doha, Qatar",
+    price: "95€",
+    image: "",
+    type: "Moteur",
+    note: "4,7 / 5",
+    description:
+      "Pratique pour partir à la découverte de Doha, DoubleTree by Hilton Doha - Al Sadd se trouve à seulement 5 min en voiture de Galerie d'art Fire Station et à 5 minutes de Grand Hamad Stadium. Cet hôtel 4 étoiles se trouve à 4,5 km de Corniche de Doha et à 7,1 km de Centre commercial Villagio Mall. Avec un ameublement personnalisé, les 139 chambres de l'établissement vous invitent à la détente et comprennent un minibar et une télévision LCD. L'accès Wi-Fi à Internet gratuit vous permet de rester en contact avec le reste du monde. Une salle de bain privée avec une baignoire et une douche séparées est à votre disposition. Vous y trouvez également des articles de toilette gratuits et un sèche-cheveux. Les équipements et services offerts par l'établissement comprennent un téléphone, mais aussi un coffre-fort (suffisamment grand pour accueillir un ordinateur portable) et un bureau",
+  },
+  {
+    id: 2,
+    name: "Hilton Doha The Pearl Hotel & Residences ",
+    adress: "Abraj Quartier Pearl Boulevard Tower AQ-07, Doha, Qatar",
+    price: "150€",
+    image: "",
+    type: "Moteur",
+    note: "4,5 / 5",
+    description:
+      " Avec une décoration personnalisée, les 414 chambres de l'établissement vous invitent à la détente et comprennent un réfrigérateur et un micro-ondes. Une télévision à écran plat 55 pouces avec chaînes par câble est à votre disposition pour de purs moments de détente, alors qu'un accès au réseau l'accès Wi-Fi à Internet gratuit vous permet de rester en contact avec le reste du monde. Une salle de bain privée avec une baignoire et une douche séparées est à votre disposition. Vous y trouvez également des articles de toilette de luxe et un sèche-cheveux. Les équipements et services offerts par l'établissement comprennent un téléphone, mais aussi un coffre-fort et un bureau. ",
+  },
+  {
+    id: 3,
+    name: "AlRayyan Hotel Doha, Curio Collection by Hilton ",
+    adress:
+      "Mall of Qatar, Doha, Qataraj Quartier Pearl Boulevard Tower AQ-07, Doha, Qatar",
+    price: "188 €",
+    image: "",
+    type: "Moteur",
+    note: "4,3 / 5 ",
+    description:
+      "En choisissant AlRayyan Hotel Doha, Curio Collection by Hilton à Al Rayyan, vous profiterez d'une liaison avec le centre commercial, à quelques pas de Centre commercial Mall of Qatar et à 14 min à pied de Stade Al Rayyan. Cet hôtel 5 étoiles se trouve à 10,4 km de Parcours de golf Education City Golf Club et à 10,9 km de Centre national de congrès du Qatar.Laissez place à la détente en rejoignant les 3 piscines extérieures de l'établissement et en profitant des nombreuses infrastructures de loisirs qui incluent un centre de remise en forme et un sauna. Cet hôtel propose également l'accès Wi-Fi à Internet gratuit, un service de conciergerie et une boutique de souvenirs/un kiosque à journaux. ",
+  },
+  {
+    id: 4,
+    name: "AlRayyan Hotel Doha, Curio Collection by Hilton ",
+    adress:
+      "Mall of Qatar, Doha, Qataraj Quartier Pearl Boulevard Tower AQ-07, Doha, Qatar",
+    price: "188 €",
+    image: "",
+    type: "Moteur",
+    note: "4,3 / 5 ",
+    description:
+      "En choisissant AlRayyan Hotel Doha, Curio Collection by Hilton à Al Rayyan, vous profiterez d'une liaison avec le centre commercial, à quelques pas de Centre commercial Mall of Qatar et à 14 min à pied de Stade Al Rayyan. Cet hôtel 5 étoiles se trouve à 10,4 km de Parcours de golf Education City Golf Club et à 10,9 km de Centre national de congrès du Qatar.Laissez place à la détente en rejoignant les 3 piscines extérieures de l'établissement et en profitant des nombreuses infrastructures de loisirs qui incluent un centre de remise en forme et un sauna. Cet hôtel propose également l'accès Wi-Fi à Internet gratuit, un service de conciergerie et une boutique de souvenirs/un kiosque à journaux. ",
+  },
+  {
+    id: 5,
+    name: "AlRayyan Hotel Doha, Curio Collection by Hilton ",
+    adress: "Tower 26, Porto Arabia Marina, The Pearl, Doha, Qatar      ",
+    price: "300 €",
+    image: "",
+    type: ["Moteur", "auditif"],
+    note: "4 / 5 ",
+    description:
+      "Cet appart'hôtel tout confort est à Doha. Les locaux disposent de 250 chambres accueillantes. Qu'ils viennent pour un voyage d'affaires ou d'agrément, tous les clients pourront rester connectés grâce à l'accès à Internet disponible dans les locaux. Retaj Inn Marina Residence dispose d'une réception ouverte 24 heures sur 24. Par ailleurs, Retaj Inn Marina Residence dispose de lits d'appoint pour enfants, à installer sur demande. Cette résidence n'accepte pas les animaux de compagnie. Les voyageurs arrivant en voiture pourront laisser leur véhicule sur les places de parking mises à leur disposition.",
+  },
+  {
+    id: 6,
+    name: "AlRayyan Hotel Doha, Curio Collection by Hilton ",
+    adress:
+      "Al Matar Street, Plot. No. 9, Street No. 850, Area No. 26, Doha    ",
+    price: "300 €",
+    image: "",
+    type: ["Moteur", "auditif"],
+    note: "4 / 5 ",
+    description:
+      "Cet appart'hôtel tout confort est à Doha. Les locaux disposent de 250 chambres accueillantes. Qu'ils viennent pour un voyage d'affaires ou d'agrément, tous les clients pourront rester connectés grâce à l'accès à Internet disponible dans les locaux. Retaj Inn Marina Residence dispose d'une réception ouverte 24 heures sur 24. Par ailleurs, Retaj Inn Marina Residence dispose de lits d'appoint pour enfants, à installer sur demande. Cette résidence n'accepte pas les animaux de compagnie. Les voyageurs arrivant en voiture pourront laisser leur véhicule sur les places de parking mises à leur disposition.",
+  },
+];
+
+export default beers;
