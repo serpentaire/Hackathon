1022e726e7080a72b0216c17a0d831458f0ff001
arouxel17 <alexandrerouxel17@gmail.com>
2022-11-24 14:57:28 +0100

    Juste création de la confirm page + son text

---
+++ frontend/src/pages/ConfirmPage/ConfirmPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ConfirmPage () {
+
+   return(
+    <div className="container">
+        <div className="confirmation-message text-center">
+            <h1>Nous avons bien reçu votre paiement, votre séjour va être préparé par nos soins ! 🌞</h1>
+        </div>
+    </div>
+   ) 
+};
+
+export default ConfirmPage;
